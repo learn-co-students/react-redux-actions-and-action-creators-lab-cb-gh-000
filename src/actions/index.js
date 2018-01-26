a9952482ae4ed781cbc11cabe154c1d12e377034
Tanuj Sharma <tanuj.goodboy77@gmail.com>
2018-01-26 18:29:51 +0000

    Done.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,37 @@
 // define your actions here!
+// define your actions here!
+export function incrementCount(){
+  return {
+    type: 'INCREMENT_COUNT'
+  }
+}export function addMovie(movie) {
+  return {
+    type: 'ADD_MOVIE',
+    movie: {
+      title: movie.title
+    }
+  }
+}
+
+export function deleteMovie(movie) {
+  return {
+    type: 'DELETE_MOVIE',
+    movie: {
+      title: movie.title
+    }
+  }
+}
+
+export function updateUsername(newName) {
+  return {
+    type: 'UPDATE_USERNAME',
+    username: newName
+  }
+}
+
+export function resetUsername() {
+  return {
+    type: 'RESET_USERNAME',
+    username: 'default'
+  }
+}
