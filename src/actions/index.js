0a2652a0e34f9ed13be5b448cbd616dec290fcfb
Ngoc Dong Pham <cpud1258@gmail.com>
2018-09-23 13:23:32 +0000

    Done.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,32 @@
 // define your actions here!
+export function addMovie(){
+  return {
+    type: 'ADD_MOVIE',
+    movie:{
+      title:'Ghostbusters'
+    }
+  }
+}
+// define your actions here!
+export function deleteMovie(){
+  return {
+    type: 'DELETE_MOVIE',
+    movie:{
+      title:'Dead Poets Society'
+    }
+  }
+}
+// define your actions here!
+export function updateUsername(username){
+  return {
+    type: 'UPDATE_USERNAME',
+    username:username
+  }
+}
+// define your actions here!
+export function resetUsername(){
+  return {
+    type: 'RESET_USERNAME',
+      username:'default'
+  }
+}
