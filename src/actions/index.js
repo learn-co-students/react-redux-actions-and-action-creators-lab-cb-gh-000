b29f9a3890b3270ab751742e7e33ad1b260c597a
Anak Agung Angga Wijaya <anggawijaya4567@gmail.com>
2019-03-09 18:09:44 +0000

    Done.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,35 @@
 // define your actions here!
+export function addMovie(movie){
+  return {
+    type: 'ADD_MOVIE',
+    movie : {
+      title: movie.title
+    }
+  };
+}
+
+//Delete an existing movie
+export function deleteMovie(){
+  return {
+    type: 'DELETE_MOVIE',
+    movie: {
+      title: 'Dead Poets Society'
+    }
+  };
+}
+
+//Update their username
+export function updateUsername(username){
+  return {
+    type: 'UPDATE_USERNAME',
+    username
+  };
+}
+
+//Reset their username to the default username of 'default'
+export function resetUsername(){
+  return {
+    type: 'RESET_USERNAME',
+    username: 'default'
+  };
+}
